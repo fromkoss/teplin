d1fcf54d589b9314b878273b4aee6dc9f029366a
fromkoss <kostiantyn.miliev@gmail.com>
2022-03-21 17:27:50 +0800

    added sections: featured-products, content-cards

---
+++ js/product-add-btn.js
@@ -0,0 +1,74 @@
+if (!customElements.get('product-add-btn')) {
+    customElements.define('product-add-btn', class ProductAddBtn extends HTMLElement {
+        constructor() {
+            super();
+            this.addEventListener('click', this._onClick.bind(this));
+        }
+
+        /**
+         *
+         * @param evt
+         * @private
+         */
+        _onClick(evt) {
+            evt.preventDefault();
+            if (this.classList.contains('loading')) return;
+            this.setAttribute('aria-disabled', true);
+            this.classList.add('loading');
+            const { id, qty, ...properties} = this.dataset;
+
+            return this._addToCart({
+                items: [
+                    {
+                        id,
+                        quantity: qty || 1,
+                        properties,
+                    }
+                ]
+            });
+        }
+
+        /**
+         *
+         * @param payload
+         * @returns {Promise<void>}
+         * @private
+         */
+        async _addToCart(payload){
+            await fetch(`${window.Shopify.routes.root}cart/add.js`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            })
+                .then(response => {
+                    return response.json();
+                }).then((response) => {
+            })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            const cart = await this._getCart().finally(() => {
+                this.classList.remove('loading');
+                this.removeAttribute('aria-disabled');
+            });;
+            console.log(cart);
+        }
+
+        /**
+         *
+         * @returns {Promise<any>}
+         * @private
+         */
+        async _getCart() {
+            return await fetch(`${window.Shopify.routes.root}cart.js`,)
+                .then(response => {
+                    return response.json();
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+    });
+}
